4702deb33d9b947f8bf00333a4e406d5f55eba82
alexkataev <39487162+alexkataev@users.noreply.github.com>
2020-08-10 14:30:59 +0300

    Added example #3 and Top View Traversal output

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import preorder as _pre
 import postorder as _post
 import inorder as _in
 import breadth_first as _bf
+import top_view as _topv
 
 
 def main():
@@ -32,12 +33,33 @@ def main():
     #                  8
     #                 /
     #                9
+    # tree.insert(1, 2, 3)
+    # tree.insert(2, right=4)
+    # tree.insert(3, 5, 6)
+    # tree.insert(6, right=7)
+    # tree.insert(7, right=8)
+    # tree.insert(8, left=9)
+
+    # EXAMPLE #3:
+    #        1
+    #      /    \
+    #     2      3
+    #      \    / \
+    #       4  7   8
+    #      /  / \
+    #     5  9   10
+    #    /         \
+    #   6           11
+    #                 \
+    #                  12
     tree.insert(1, 2, 3)
     tree.insert(2, right=4)
-    tree.insert(3, 5, 6)
-    tree.insert(6, right=7)
-    tree.insert(7, right=8)
-    tree.insert(8, left=9)
+    tree.insert(3, 7, 8)
+    tree.insert(4, left=5)
+    tree.insert(5, left=6)
+    tree.insert(7, 9, 10)
+    tree.insert(10, right=11)
+    tree.insert(11, right=12)
 
     # Below is the block of printing results of our algorithms:
     print(f'Preorder (N-L-R) (iterative): {_pre.preorder_iterative(tree.root)}')
@@ -56,6 +78,10 @@ def main():
     print()
 
     print(f'Breadth-first (levels) (iterative): {_bf.breadth_first_using_queue(tree.root)}')
+
+    print()
+
+    print(f'Top View Traversal (iterative): {_topv.top_view(tree.root)}')
 
 
 if __name__ == '__main__':
